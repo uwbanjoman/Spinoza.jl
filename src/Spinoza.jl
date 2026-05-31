3e9d93ff7d0efce2308ecdd8fe5fa0a2c47162e1
Jan Bouwman <49314613+uwbanjoman@users.noreply.github.com>
2026-05-31 11:57:01 +0200

    Re-export functions from QuantumFisher in Spinoza module

--- src/Spinoza.jl
+++ src/Spinoza.jl
@@ -1,2 +1,9 @@
 module Spinoza
+
+using QuantumFisher
+
+# Re-export from QuantumFisher
+using QuantumFisher: density_matrix, vacuum_state, fisher_tensor,
+                     bures_distance, evolve_exact, kk_hamiltonian
+
 end # module Spinoza
